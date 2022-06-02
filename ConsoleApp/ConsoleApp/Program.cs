26e35baaedf9cee52ec0c138d1e011c17aeda1c6
MiniFid <nathan.d.fidler@gmail.com>
2022-06-02 12:56:21 -0600

    do while loop

---
+++ ConsoleApp/ConsoleApp/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp
+{
+    class Program
+    {
+        static void Main()
+        {
+            bool stopLoop = false;
+            do
+            {
+                Console.WriteLine("Please type true");
+                if (Console.ReadLine() == "true")
+                {
+                    Console.WriteLine("loop ends");
+                    stopLoop = true;
+                }
+                else
+                {
+                    Console.WriteLine("you misspelled true, try again.");
+                }
+            }
+            while (!stopLoop);
+            Console.Read();
+
+        }
+    }
+}
