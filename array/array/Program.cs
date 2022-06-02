b2524ed946b5bb7a7669d9f1d6e315882f2ed4c7
MiniFid <nathan.d.fidler@gmail.com>
2022-06-02 16:54:16 -0600

    finished assignment

--- array/array/Program.cs
+++ array/array/Program.cs
@@ -10,7 +10,7 @@ namespace array
     {
         static void Main()
         {
-            int[] numArray = new int[] { 0, 5, 6, 4, 2, 14 };
+            //asks for an index of array myArray and displays value
             string[] myArray = new string[] { "George", "Fred", "Bob"};
             Console.WriteLine("please choose which index you want to change from the below array (0-2) \nGeorge, Fred, Bob");
             int index = Convert.ToInt32(Console.ReadLine());
@@ -20,11 +20,12 @@ namespace array
             }
             else
             {
-                Console.WriteLine("Please enter the new value");
-                myArray[index] = Console.ReadLine();
-                Console.WriteLine("The value at index " + index + " has been changed");
+                Console.WriteLine("The value at index " + index + " " + myArray[index]);
             }
+
+            //asks for an index of array numArray and displays value
             Console.WriteLine("Select an index (0-5)");
+            int[] numArray = new int[] { 0, 5, 6, 4, 2, 14 };
             int numIndex = Convert.ToInt32(Console.ReadLine());
             if (numIndex >= numArray.Length)
             {
@@ -33,6 +34,19 @@ namespace array
             else
             {
                 Console.WriteLine(numArray[numIndex]);
+            }
+
+            //asks for an index for the list myList and displays the value
+            List<string> myList = new List<string>() { "text", "more text", "even more text" };
+            Console.WriteLine("Enter an index (0-2).");
+            int listIndex = Convert.ToInt32(Console.ReadLine());
+            if (listIndex >= myList.Count)
+            {
+                Console.WriteLine("There is no index at that location");
+            }
+            else
+            {
+                Console.WriteLine(myList[listIndex]);
             }
             Console.Read();
         }
