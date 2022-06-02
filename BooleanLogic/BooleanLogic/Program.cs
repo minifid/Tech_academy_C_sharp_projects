8fd2c1155311fd3c557d21b0864930050170b3e7
MiniFid <nathan.d.fidler@gmail.com>
2022-06-02 03:31:12 -0600

    Boolean Logic

---
+++ BooleanLogic/BooleanLogic/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BooleanLogic
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("What is your age?");
+            int age = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Have you ever had a DUI");
+            string dui = Console.ReadLine().ToLower();
+            bool hasDUI = (dui == "y" || dui == "yes" || dui == "true" || dui == "t");
+
+            Console.WriteLine("How many speeding tickets do you have");
+            int tickets = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Results");
+            Console.WriteLine("What is your age\n" + age);
+            Console.WriteLine("Have you ever had a DUI?\n" + hasDUI);
+            Console.WriteLine("How many speeding tickets do you have.\n" + tickets);
+            bool canHaveLicense = (age > 15 && hasDUI == false && tickets <= 3);
+            Console.WriteLine("Qualified\n" + canHaveLicense);
+
+            Console.Read();
+        }
+    }
+}
