61f96342c92fb86b873bc947c040c3638ac248a4
MiniFid <nathan.d.fidler@gmail.com>
2022-06-02 18:28:58 -0600

    calling method assignment

---
+++ CallingMethods/CallingMethods/MyClass.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CallingMethods
+{
+    class MyClass
+    {
+        public static int addition(int x)
+        {
+            return x + 10;
+        }
+        public static int multiply(int x)
+        {
+            return x * 10;
+        }
+        public static int subtract(int x)
+        {
+            return x - 10;
+        }
+    }
+}
---
+++ CallingMethods/CallingMethods/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CallingMethods
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("enter a number to be used in math operations.");
+            int num = Convert.ToInt32(Console.ReadLine());
+            //applies the corresponding math operation to the user input
+            Console.WriteLine(MyClass.multiply(num));
+            Console.WriteLine(MyClass.addition(num));
+            Console.WriteLine(MyClass.subtract(num));
+
+            Console.Read();
+        }
+    }
+}
