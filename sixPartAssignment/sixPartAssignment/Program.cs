8fe5c23c0611e424498a97162425b93011fb1a3f
MiniFid <nathan.d.fidler@gmail.com>
2022-06-02 18:57:58 -0600

    used lists

--- sixPartAssignment/sixPartAssignment/Program.cs
+++ sixPartAssignment/sixPartAssignment/Program.cs
@@ -10,10 +10,12 @@ namespace sixPartAssignment
     {
         static void Main()
         {
+            //part 1
             //first string will add the users text to the end of each of the string in myArray1
             string[] myArray1 = new string[] { "first", "second", "third" };
             Console.WriteLine("Please enter text to append to all elements of the array");
             string uInput = Console.ReadLine();
+            // part 3
             for (int i=0; i < myArray1.Length; i++)
             {
                 myArray1[i] = myArray1[i] + uInput;
@@ -22,17 +24,20 @@ namespace sixPartAssignment
             {
                 Console.WriteLine(myArray1[i]);
             }
+            //end part 3
+            //end part 1
 
-            //will use user input to search through myArray2 and returns the first element that matches
-            string[] myArray2 = new string[] { "George", "Fred", "Nathan" };
+            //part 4
+            //will use user input to search through myList and returns the first element that matches
+            List<string> myList = new List<string>() { "George", "Fred", "Nathan" };
             Console.WriteLine("Please enter text to search by.");
             string search2 = Console.ReadLine().ToLower();
             bool noResults2 = true;
-            for (int i = 0; i < myArray2.Length; i++)
+            for (int i = 0; i < myList.Count; i++)
             {
-                if(myArray2[i].ToLower().Contains(search2))
+                if(myList[i].ToLower().Contains(search2))
                 {
-                    Console.WriteLine(myArray2[i] + " at index " + i);
+                    Console.WriteLine(myList[i] + " at index " + i);
                     if (noResults2) { noResults2 = false; }
                     break;
                 }
@@ -41,17 +46,19 @@ namespace sixPartAssignment
             {
                 Console.WriteLine("No elements in the array match your search.");
             }
+            //end part 4
 
-            //will use user input to search through myArray3 and returns all matches
-            string[] myArray3 = new string[] { "George", "Fred", "Nathan", "George" };
+            //part 5
+            //will use user input to search through myList2 and returns all matches
+            List<string> myList2 = new List<string> { "George", "Fred", "Nathan", "George" };
             Console.WriteLine("Please enter text to search by.");
             string search3 = Console.ReadLine().ToLower();
             bool noResults3 = true;
-            for (int i = 0; i < myArray3.Length; i++)
+            foreach (string value in myList)
             {
-                if (myArray3[i].ToLower().Contains(search3))
+                if (value.ToLower().Contains(search3))
                 {
-                    Console.WriteLine(myArray3[i] + " at index " + i);
+                    Console.WriteLine(value);
                     if (noResults3) { noResults3 = false; }
                     
                 }
@@ -60,23 +67,27 @@ namespace sixPartAssignment
             {
                 Console.WriteLine("No elements in the array match your search.");
             }
+            //end part 5
 
-            //will go through myArray3 and say if it is a duplicate or not
-            Console.WriteLine("\n displays duplicates in myArray3");
+            //part 6
+            //will go through myList2 and say if it is a duplicate or not
+            Console.WriteLine("\ndisplays duplicates in myList2");
+            int duplicateCount = 0;
             bool duplicate = false;
-            for (int i=0; i<myArray3.Length; i++)
+            foreach (string value in myList2)
             {
-                duplicate = false;
-                for (int j=i+1; j<myArray3.Length; j++)
+                duplicateCount = 0;
+                foreach (string value2 in myList2)
                 {
-                    if (myArray3[i] == myArray3[j])
+                    if (value == value2)
                     {
-                        duplicate = true;
-                        break;
+                        duplicateCount++;
                     }
                 }
-                Console.WriteLine("Is " + myArray3[i] + " at index " + i + " a duplicate? " + duplicate);
+                _ = duplicateCount <= 1 ? duplicate = false : duplicate = true;
+                Console.WriteLine("Is " + value + " a duplicate? " + duplicate);
             }
+            //end part 6
 
             Console.Read();
         }
