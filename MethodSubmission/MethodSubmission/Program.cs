ae73a0216e9f141c45860cfa53034d7f17834c61
MiniFid <nathan.d.fidler@gmail.com>
2022-06-02 20:06:33 -0600

    method submissions assignment

---
+++ MethodSubmission/MethodSubmission/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodSubmission
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter a number");
+            string uInput = Console.ReadLine();// created a variable so I can reuse it
+            try
+            {
+                int num1 = Convert.ToInt32(uInput);
+            }
+            catch (FormatException)
+            {
+                Console.WriteLine("first entry must be a number");
+                Console.Read();
+                Environment.Exit(0);
+            }
+           
+            Console.WriteLine("Please hit enter or insert a number, then hit enter");
+
+
+            try
+            {
+                int num1 = Convert.ToInt32(uInput); //QUESTION is there a way to keep variables created within try/catch blocks? so i dont have to create the same variable multiple times
+
+                int num2 = Convert.ToInt32(Console.ReadLine());
+                myClass.addition(num1, num2);
+            }
+            catch (FormatException)
+            {
+                int num1 = Convert.ToInt32(uInput);
+                myClass.addition(num1);
+            }
+            
+            Console.Read();
+        }
+    }
+}
---
+++ MethodSubmission/MethodSubmission/myClass.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodSubmission
+{
+    class myClass
+    {
+        public static int addition(int x, int y = 5)
+        {
+            return x+y;
+        }
+    }
+}
