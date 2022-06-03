20a805871771f792d2c7b1415f5fc444d774e7b0
MiniFid <nathan.d.fidler@gmail.com>
2022-06-02 19:33:22 -0600

    more method practice

---
+++ MainMethod/MainMethod/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MainMethod
+{
+    class Program
+    {
+        static void Main()
+        {
+            int num = myClass.addition(5);
+            Console.WriteLine(num);
+            int num2 = myClass.addition(1.5m);
+            Console.WriteLine(num2);
+            int num3 = myClass.addition("5");
+            Console.WriteLine(num3);
+            int num4 = myClass.addition("test");
+            Console.WriteLine(num4);
+
+            Console.Read();
+
+        }
+    }
+}
---
+++ MainMethod/MainMethod/myClass.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MainMethod
+{
+    class myClass
+    {
+        //accepts int returns int
+        public static int addition(int x)
+        {
+            return x + 10;
+        }
+
+        //accepts decimal returns int
+        public static int addition(decimal x)
+        {
+            return Convert.ToInt32(x + 5);
+        }
+
+        //accepts string returns int returns 0 if given text instead of a number
+        public static int addition(string x)
+        {
+            try
+            {
+            return Convert.ToInt32(x + 5);
+            }
+            catch (FormatException)
+            {
+                Console.WriteLine("cannot convert text to int returning 0");
+                return 0;
+            }
+        }
+    }
+}
