1d6b0377fbe3576046c7d3e0ca5f9f39eb23d321
MiniFid <85532592+minifid@users.noreply.github.com>
2022-07-21 14:55:14 -0600

    Add files via upload

---
+++ MethodsAndObjects/MethodsAndObjects/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodsAndObjects
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Employee employee = new Employee() { FirstName = "Sample", LastName = "Student" }; //Instatiates the object Employee with the name employee and sets first name as sample and last and student
+            employee.SayName();
+            Console.Read();
+        }
+    }
+}
