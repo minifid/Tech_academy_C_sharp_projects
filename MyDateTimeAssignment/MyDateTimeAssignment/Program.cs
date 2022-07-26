b0d3bd906b77398e0d615775f37d3fea507e6ca3
MiniFid <nathan.d.fidler@gmail.com>
2022-07-26 15:27:31 -0600

    dateTime assignment

---
+++ MyDateTimeAssignment/MyDateTimeAssignment/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyDateTimeAssignment
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(DateTime.Now);
+            Console.WriteLine("Enter a number");
+
+            double num = Convert.ToDouble(Console.ReadLine());
+            
+            DateTime currentTime = DateTime.Now;
+            DateTime futureHours = currentTime.AddHours(num);
+            
+            Console.WriteLine(futureHours);
+            Console.Read();
+        }
+    }
+}
