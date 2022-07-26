4ecf9fa515f19c9c6e0f5c0de8abe26923753cc1
MiniFid <nathan.d.fidler@gmail.com>
2022-07-26 16:41:12 -0600

    new assignment

---
+++ Constructor/Constructor/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Constructor
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            const string school = "Tech Acadamey";
+            var student = "Nathan";
+            chaining name = new chaining(student);
+            Console.WriteLine(name.LastName);
+            Console.Read();
+        }
+    }
+}
---
+++ Constructor/Constructor/chaining.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Constructor
+{
+    internal class chaining
+    {
+        public chaining(string FirstName): this(FirstName, "Student")
+        {
+        }
+        public chaining(string firstName, string lastName)
+        {
+            this.FirstName = firstName;
+            this.LastName = lastName;
+        }
+
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+    }
+}
