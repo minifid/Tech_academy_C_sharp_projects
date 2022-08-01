d4eaccfe5585b19a836b381adf043896be5c2293
MiniFid <nathan.d.fidler@gmail.com>
2022-08-01 12:07:14 -0600

    update

--- Blackjack/Blackjack/Program.cs
+++ Blackjack/Blackjack/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Data;
 using System.Data.SqlClient;
 using System.IO;
@@ -14,7 +15,20 @@ namespace Blackjack
             DateTime dateTime = new DateTime(1995, 5, 23, 8, 32, 45);
             Console.WriteLine("Welcome to the Grand Hotel and Casino. Let's start by telling me your name.");
             string playerName = Console.ReadLine();
-
+            if (playerName.ToLower() == "admin")
+            {
+                List<ExceptionEntity> Exceptions = ReadExceptions();
+                foreach (var exception in Exceptions)
+                {
+                    Console.Write(exception.Id + " | ");
+                    Console.Write(exception.ExceptionType + " | ");
+                    Console.Write(exception.ExceptionMessage + " | ");
+                    Console.Write(exception.TimeStamp + " | ");
+                    Console.WriteLine();
+                }
+                Console.Read();
+                return;
+            }
             bool validAnswer = false;
             int bank = 0;
             while (!validAnswer)
@@ -45,7 +59,7 @@ namespace Blackjack
                     }
                     catch (FraudException ex)
                     {
-                        Console.WriteLine("Security! Kick this person out.");
+                        Console.WriteLine(ex.Message);
                         UpdateDbWithException(ex);
                         Console.ReadLine();
                         return;
@@ -66,9 +80,12 @@ namespace Blackjack
 
         private static void UpdateDbWithException(Exception ex)
         {
-            string connectionString = @"Data Source = (localdb)\MSSQLLocalDB; Initial Catalog = BlackJackGame; Integrated Security = True; Connect Timeout = 30; Encrypt = False; TrustServerCertificate = False; ApplicationIntent = ReadWrite; MultiSubnetFailover = False";
+            string connectionString = @"Data Source = (localdb)\MSSQLLocalDB; Initial Catalog = BlackJackGame; 
+                                        Integrated Security = True; Connect Timeout = 30; 
+                                        Encrypt = False; TrustServerCertificate = False; 
+                                        ApplicationIntent = ReadWrite; MultiSubnetFailover = False";
 
-            string queryString = @"INSERT INTO dbo.Table (ExceptionType, ExceptionMessage, TimeStamp) VALUES
+            string queryString = @"INSERT INTO Exceptions (ExceptionType, ExceptionMessage, TimeStamp) VALUES
                                     (@ExceptionType, @ExceptionMessage, @TimeStamp)";
 
             using (SqlConnection connection = new SqlConnection(connectionString))
@@ -86,6 +103,38 @@ namespace Blackjack
                 command.ExecuteNonQuery();
                 connection.Close();
             }
+        }
+        private static List<ExceptionEntity> ReadExceptions()
+        {
+            string connectionString = @"Data Source = (localdb)\MSSQLLocalDB; Initial Catalog = BlackJackGame; 
+                                        Integrated Security = True; Connect Timeout = 30; 
+                                        Encrypt = False; TrustServerCertificate = False; 
+                                        ApplicationIntent = ReadWrite; MultiSubnetFailover = False";
+            string queryString = @"Select Id, ExceptionType, ExceptionMessage, TimeStamp From Exceptions";
+
+            List<ExceptionEntity> Exceptions = new List<ExceptionEntity>();
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                SqlCommand command = new SqlCommand(queryString, connection);
+
+                connection.Open();
+
+                SqlDataReader reader = command.ExecuteReader();
+
+                while (reader.Read())
+                {
+                    ExceptionEntity exception = new ExceptionEntity();
+                    exception.Id = Convert.ToInt32(reader["Id"]);
+                    exception.ExceptionType = reader["ExceptionType"].ToString();
+                    exception.ExceptionMessage = reader["ExceptionMessage"].ToString();
+                    exception.TimeStamp = Convert.ToDateTime(reader["TimeStamp"]);
+                    Exceptions.Add(exception);
+                }
+                connection.Close();
+            }
+
+            return Exceptions;
+
         }
     }
 }
